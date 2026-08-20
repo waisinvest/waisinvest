570e937503764f0c32ded0b2581ee215c23700dd
waisinvest <herobonnie.poon@gmail.com>
2026-08-20 17:53:30 -0400

    Load GNRC radar update after prior WAIS overlays

--- market-data.js
+++ market-data.js
@@ -56,3 +56,4 @@ document.write('<script src="wais-canonical-state-20260820.js?v=20260820a"></scr
 document.write('<script src="wais-radar-update-20260820-1150.js?v=20260820b"></script>');
 document.write('<script src="wais-radar-update-20260820-1257.js?v=20260820c"></script>');
 document.write('<script src="wais-radar-update-20260820-1656.js?v=20260820d"></script>');
+document.write('<script src="wais-radar-update-20260820-1752.js?v=20260820e"></script>');
